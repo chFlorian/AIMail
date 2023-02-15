a2d7b4e090c8d272e7e1efdf996adc64db7dedf9
Florian Schweizer <post.an.flo@gmail.com>
2023-02-15 10:50:08 +0100

    Initial Commit

---
+++ AIMail/AIMailApp.swift
@@ -0,0 +1,12 @@
+// Created by Florian Schweizer on 15.02.23
+
+import SwiftUI
+
+@main
+struct AIMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
